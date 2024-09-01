fd597c8edc1ba08254de5b55960271aff0504ce2
Dmitry Jemerov <yole@hey.com>
2024-09-01 10:16:09 +0200

    K2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.serialization") version "1.9.21"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
     application
 }
 
